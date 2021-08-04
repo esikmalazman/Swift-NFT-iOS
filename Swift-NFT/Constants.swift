f418d00ce7243a972dc51d0bcb6cc2651c01d8ae
Ikmal Azman <opikmalazman@gmail.com>
2021-08-04 08:49:01 +0800

    Add In-App Purchase capabilities, register app on developer account, add IAP package in App Store Connect

---
+++ Swift-NFT/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Swift-NFT
+//
+//  Created by Ikmal Azman on 04/08/2021.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct ProductID {
+        static let swifties_100 = "com.esikmalazman.Swift_NFT.basic"
+        static let swifties_300 = "com.esikmalazman.Swift_NFT.standard"
+        static let swifties_1000 = "com.esikmalazman.Swift_NFT.premium"
+        static let swifties_3000 = "com.esikmalazman.Swift_NFT.super_premium"
+    }
+}
