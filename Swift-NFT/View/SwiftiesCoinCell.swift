462223934ff5eb300d963ec9a286172816e8eb05
Ikmal Azman <opikmalazman@gmail.com>
2021-08-07 09:36:51 +0800

    add dropdown shadow for each ccell

--- Swift-NFT/View/SwiftiesCoinCell.swift
+++ Swift-NFT/View/SwiftiesCoinCell.swift
@@ -21,9 +21,37 @@ class SwiftiesCoinCell: UICollectionViewCell {
         return imageView
     }()
     
+    override func layoutSubviews() {
+        configureLayout()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         self.backgroundView = cellBackgroundImage
-     
+        
+    }
+    override func prepareForReuse() {
+        configureForReuse()
+    }
+    func configureLayout(){
+        self.contentView.layer.cornerRadius = 15
+        self.contentView.layer.borderWidth = 0
+        self.contentView.layer.borderColor = UIColor.clear.cgColor
+        self.contentView.layer.masksToBounds = true
+        
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+        self.layer.shadowRadius = 0
+        self.layer.shadowOpacity = 0.01
+        self.layer.masksToBounds = false
+        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.contentView.layer.cornerRadius).cgPath
+    }
+    
+    func configureForReuse(){
+        
+        coinsImage.image = nil
+        coinsValue.text = nil
+        price.text = nil
+        
     }
 }
