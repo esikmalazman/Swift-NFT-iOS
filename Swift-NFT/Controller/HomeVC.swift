bea8d36a4314363b3b9957ee819620f2303bff25
Ikmal Azman <opikmalazman@gmail.com>
2021-08-07 09:36:01 +0800

    add fonts

--- Swift-NFT/Controller/HomeVC.swift
+++ Swift-NFT/Controller/HomeVC.swift
@@ -8,13 +8,16 @@
 import UIKit
 
 class HomeVC : UIViewController {
-
-
+    
+    
+    @IBOutlet weak var cardName: UILabel!
+    @IBOutlet weak var cardNumber: UILabel!
     @IBOutlet weak var swiftiesCoin: UILabel!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         didHaveSwifties()
+        setupView()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,12 +29,17 @@ class HomeVC : UIViewController {
             swiftiesCoin.text = "\(swiftWallet)"
         }
     }
-
-    @IBAction func topupPressed(_ sender: UIButton) {
+    
+    func setupView() {
+        cardName.text = "Tim Swift"
+        cardNumber.text = "SW32 2013 0012 6745"
         
-    
+        navigationController?.navigationBar.largeTitleTextAttributes =
+            [
+                NSAttributedString.Key.foregroundColor : UIColor(red: 1.0, green: 0.6, blue: 0.14, alpha: 1),
+                NSAttributedString.Key.font : UIFont(name: "Inter-SemiBold", size: 40)!
+            ]
+        navigationController?.navigationBar.tintColor = .black
     }
-    
-    
 }
 
